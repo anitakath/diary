c8e4bdf4d50a20ff317825e11f91352291ba8b96
anitakath <wagner.annekathrin@gmx.de>
2024-02-01 13:09:51 +0100

    saveAndUpdateUser function at pages/index

--- context/RedditContext.js
+++ context/RedditContext.js
@@ -7,31 +7,28 @@ import { createContext, useState, useEffect } from "react";
 export const RedditContext = createContext();
 
 export const RedditProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState(null);
 
+  //fetchet hits the endpoint (api/get-posts) and retrieves its result as json. result is named data
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-    useEffect(()=>{
-        const {user} = supabase.auth.getSession() || {user: null}
+  useEffect(() => {
+    const { user } = supabase.auth.getSession() || { user: null };
 
-        setCurrentUser(user)
+    setCurrentUser(user);
 
-        //console.log({user}, 'HIIIII :>')
+    //console.log({user}, 'HIIIII :>')
 
+    supabase.auth.onAuthStateChange((_event, authSession) => {
+      setCurrentUser(authSession);
+    });
+  }, []);
 
-        supabase.auth.onAuthStateChange((_event, authSession) =>{
-            setCurrentUser(authSession)
-        })
+  //console.log(currentUser);
 
-    }, [])
-
-    //console.log(currentUser);
-    
-
-
-    return(
-        <RedditContext.Provider
-        value={{currentUser}}>
-            {children}
-        </RedditContext.Provider>
-    )
+  return (
+    <RedditContext.Provider value={{ currentUser , fetcher}}>
+      {children}
+    </RedditContext.Provider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -20,16 +20,24 @@ import { supabase } from "@/services/supabaseClient";
 import { current } from "@reduxjs/toolkit";
 
   //fetchet hits the endpoint (api/get-posts) and retrieves its result as json. result is named data
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+//const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 
 export default function Home() {
 
-  const {currentUser} = useContext(RedditContext)
+  const {currentUser, fetcher} = useContext(RedditContext)
  
+  
 
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
+
+
+  const [myPosts, setMyPosts] = useState();
+
+  const { data, error } = useSWR("/api/get-post", fetcher, {
+    refreshInterval: 200,
+  });
 
  
   useEffect(()=>{
@@ -39,12 +47,63 @@ export default function Home() {
     }
   }, [currentUser])
 
+ 
 
+  const saveAndUpdateUser = async () =>{
+    if (!currentUser) return;
+
+    
+    // Überprüfe, ob der Benutzer bereits in der Datenbank existiert
+    const { data: existingUserData } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", currentUser.user.identities[0].identity_data.email);
+
+      console.log(existingUserData)
+
+    if (existingUserData.length > 0) {
+      // Der Benutzer existiert bereits - führe ein Update durch
+      await supabase
+        .from("users")
+        .update({
+          name: currentUser.user.identities[0].identity_data.name,
+          profileImage: currentUser.user.identities[0].identity_data.picture,
+        })
+        .eq("email", currentUser.user.identities[0].identity_data.email);
+    } else {
+      // Der Benutzer existiert noch nicht - füge ihn hinzu
+      await supabase.from("users").upsert({
+        email: currentUser.user.identities[0].identity_data.email,
+        name: currentUser.user.identities[0].identity_data.name,
+        profileImage: currentUser.user.identities[0].identity_data.picture,
+      });
+    }
+
+    /*
+
+    await supabase.from("users").upsert({
+      email: currentUser.user.identities[0].identity_data.email,
+      name: currentUser.user.identities[0].identity_data.name,
+      profileImage: currentUser.user.identities[0].identity_data.picture,
+    })*/
+
+    const { data } = await supabase
+      .from("users") // der erstellte table auf supabase.com...
+      .select("*");
+
+    console.log(data);
+  }
+
+  useEffect(() =>{
+    if (!data) return
+    setMyPosts(data.data)
+  }, [data])
+
+  useEffect(()=>{
+    saveAndUpdateUser()
+    console.log('Save user')
+  }, [])
   
-
-  const [myPosts, setMyPosts] = useState()
-
-  const {data, error} = useSWR('/api/get-post', fetcher, {refreshInterval: 200} )
 
 
   
